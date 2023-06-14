7e81bdd880ae9a5f1b7107c06927ab03e5c798b0
Александр Гурьев <arh-tehnik@mail.ru>
2023-06-14 18:46:41 +0300

    Добавьте файлы проекта.

---
+++ 14062023/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14062023
+{
+    interface Ia
+    {
+        string Ia(int n);
+    }
+    interface Ib
+    {
+        string Ib(int n);
+        void b2();
+
+    }
+    interface Ic :Ia,Ib
+    {
+        void c2(int n);
+
+    }
+    class In : Ic
+    {
+        public void b2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void c2(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Ia(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Ib(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            In a = new In();
+            Ia i = new In();
+            
+
+
+            //стандартные интерфейсы 
+
+        }
+
+    }
+}
